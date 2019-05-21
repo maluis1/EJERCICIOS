6cb01f5f0d8b8ae468b0469ec6d188651f8caca2
maluis1 <50213977+maluis1@users.noreply.github.com>
2019-05-20 23:12:02 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creado por: miguel Alejandro luis Lopez
+            //Fecha: 15-05-2019
+            //ficha :1804643
+            int a = 10, b = 3,c =1,d, e;
+            float x, y;
+            x = a / b;
+            // c=a<b && c;
+           if ((a<b) && (a < c));
+            {
+                //sale error por que es una comparacion y para evitar esos errores podemos colocar una condicion con un if. 
+            }
+            d = a + b++;
+            e = ++a - b;
+            y = (float)a / b;
+
+        }
+    }
+}
